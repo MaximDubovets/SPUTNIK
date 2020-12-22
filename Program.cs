8b55c664e9768c94e598ea7176997bc159355e43
Maxim <maksim.dubovets.02@gmail.com>
2020-12-22 19:08:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");                             //СУММА ПРОМЕЖУТКА ЧИСЕЛ
+            Console.WriteLine("Введите минимальное число");
+            int n_min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальное число");
+            int n_max = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = n_min; i <= n_max; i++)
+            {
+                sum = sum + i;
+                //Console.WriteLine(i);
+            }
+            Console.WriteLine("Сумма: " + sum);
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadLine();
+        }
+    }
+}
